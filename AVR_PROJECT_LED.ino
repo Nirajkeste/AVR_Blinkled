ca5d71f61152e52806a1b5f42a43bfb356cc8833
Nirajkeste <nirajkeste56@gmail.com>
2025-04-23 20:29:19 +0530

    Add files via upload

--- AVR_PROJECT_LED.ino
+++ AVR_PROJECT_LED.ino
@@ -1,10 +1,3 @@
-
-//Here in the code we are going to blink the led using avr programming. I know that blinking led is very easy task using arduino. but then why it is reuired of avr ?
-// since arduino support to atmega 328p chip which is embeede in it and you can easily make code using ide but when you move to real application you are not going to use ide instead
-// you have to use different software to run different chips like atmega 32 which can not run through ide if you use something called bootloader .
-// so here i think basic information is enough why to use ave now lets move to the code.
-
-
 #include <avr/io.h> // include the avr library
 
 #include <util/delay.h> // include the delay library
@@ -19,7 +12,7 @@
 int main (void){
 
   DDRB =  DDRB | (1<<3);   // MAKING PB3 AS OUT PUT PIN
-  DDRB = DDRB & (~(1<<2));  // MAKING PB2 AS INPUT PIN
+  DDRB = DDRB & (~(1<<2));  // MAKING PB2 AS INPUT PIN here for input we are using switch that is connected pin of PB2 in circuit diagram the connection shown also uno in pin out 
   PORTB = PORTB | (1<<2);    
 
   int pin_status;         // MAKING PIN STATUS TO MAKE LED ON AND OFF
